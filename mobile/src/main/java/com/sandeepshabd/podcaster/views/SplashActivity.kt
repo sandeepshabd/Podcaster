6a206fa40438d0817da7b535ff32ee0cc9838cf6
Sandeep Shabd <sandeep.shabd@gm.com>
2017-12-22 06:49:37 -0500

    added initial code

---
+++ mobile/src/main/java/com/sandeepshabd/podcaster/views/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.sandeepshabd.podcaster.views
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import com.sandeepshabd.podcaster.R
+
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+    }
+
+}
