ad8b306e665673ba0468825fe714d23668e9decf
Sandeep Shabd <sandeep.shabd@gm.com>
2017-12-29 09:08:31 -0500

    adding okhttp to get xml and defining elements

---
+++ mobile/src/main/java/com/sandeepshabd/podcaster/handler/ConstantHandler.kt
@@ -0,0 +1,18 @@
+package com.sandeepshabd.podcaster.handler
+
+/**
+ * Created by sandeepshabd on 12/29/17.
+ */
+class ConstantHandler {
+    companion object {
+
+        //XML items
+        const val ITEM = "item"
+        const val TITLE = "title"
+        const val SUBTITLE = "itunes:subtitle"
+        const val PUB_DATE = "pubDate"
+        const val DURATION = "itunes:duration"
+        const val IMAGE = "itunes:image"
+        const val AUDIO = "enclosure"
+    }
+}
