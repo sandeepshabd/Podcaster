e5511e2b7977ecd33e11acc3a93870fc83693299
Sandeep Shabd <sandeep.shabd@gm.com>
2017-12-30 16:49:46 -0500

    displaying data in recyclerview

--- mobile/src/main/java/com/sandeepshabd/podcaster/adapter/PodcastViewAdapter.kt
+++ mobile/src/main/java/com/sandeepshabd/podcaster/adapter/PodcastViewAdapter.kt
@@ -1,23 +1,34 @@
 package com.sandeepshabd.podcaster.adapter
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.sandeepshabd.podcaster.R
 import com.sandeepshabd.podcaster.models.RSSItem
+import com.squareup.picasso.Picasso
 
 /**
  * Created by sandeepshabd on 12/30/17.
  */
 
-class PodcastViewAdapter(private var rssItems: ArrayList<RSSItem>):RecyclerView.Adapter<PodcastViewHolder>(){
+class PodcastViewAdapter(private var context:Context,
+                         private var rssItems: ArrayList<RSSItem>):RecyclerView.Adapter<PodcastViewHolder>(){
 
-    override fun onBindViewHolder(holder: PodcastViewHolder?, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(holder: PodcastViewHolder, position: Int) {
+        holder.podcastTitle.text = rssItems.get(position).title
+        holder.podcastDuration.text = rssItems.get(position).duration
+        holder.podcastPubDate.text = rssItems.get(position).pubDate
+        Picasso.with(context)
+                .load(rssItems.get(position).imageSource)
+                .resize(50, 50)
+                .centerCrop().into(holder.podcastPoster)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PodcastViewHolder {
-        val movieCardView = LayoutInflater.from(parent.context)
+        context = parent.context
+        val movieCardView = LayoutInflater.from(context)
                 .inflate(R.layout.movie_card_view, parent, false)
         return PodcastViewHolder(movieCardView)
     }
--- mobile/src/main/java/com/sandeepshabd/podcaster/views/DisplayDataActivity.kt
+++ mobile/src/main/java/com/sandeepshabd/podcaster/views/DisplayDataActivity.kt
@@ -2,10 +2,14 @@ package com.sandeepshabd.podcaster.views
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import com.sandeepshabd.podcaster.R
+import com.sandeepshabd.podcaster.adapter.PodcastViewAdapter
 import com.sandeepshabd.podcaster.handler.ConstantHandler
 import com.sandeepshabd.podcaster.models.RSSItem
 import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.find
 import org.jetbrains.anko.info
 
 class DisplayDataActivity : AppCompatActivity(), AnkoLogger {
@@ -15,5 +19,10 @@ class DisplayDataActivity : AppCompatActivity(), AnkoLogger {
         setContentView(R.layout.activity_display_data)
         var displayRssData = intent.getParcelableArrayListExtra<RSSItem>(ConstantHandler.RSS_DATA)
         info("got data in Diplay. Size of data:" + displayRssData.size)
+        var podcastRecycler = find<RecyclerView>(R.id.podcastListingRecyclerView)
+        var linearLayout = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        podcastRecycler.layoutManager = linearLayout
+        podcastRecycler.setHasFixedSize(false)
+        podcastRecycler.adapter = PodcastViewAdapter(this, displayRssData)
     }
 }
